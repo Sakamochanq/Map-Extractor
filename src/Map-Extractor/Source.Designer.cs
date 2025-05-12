eb740fa4b3669f877a1e553a118c181ec38a0f78
Sakamochanq <Sakamochan.q@outlook.jp>
2025-05-12 17:55:44 +0900

    add: UIの作成

--- src/Map-Extractor/Source.Designer.cs
+++ src/Map-Extractor/Source.Designer.cs
@@ -28,23 +28,92 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Source));
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.LoadShapeButton = new System.Windows.Forms.Button();
+            this.toolStrip1 = new System.Windows.Forms.ToolStrip();
+            this.CanvasBox = new System.Windows.Forms.PictureBox();
+            this.PathLabel = new System.Windows.Forms.ToolStripLabel();
+            this.panel1.SuspendLayout();
+            this.toolStrip1.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CanvasBox)).BeginInit();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panel1.Controls.Add(this.LoadShapeButton);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(142, 361);
+            this.panel1.TabIndex = 0;
+            // 
+            // LoadShapeButton
+            // 
+            this.LoadShapeButton.Location = new System.Drawing.Point(10, 10);
+            this.LoadShapeButton.Name = "LoadShapeButton";
+            this.LoadShapeButton.Size = new System.Drawing.Size(121, 34);
+            this.LoadShapeButton.TabIndex = 0;
+            this.LoadShapeButton.Text = "読み込み（F）";
+            this.LoadShapeButton.UseVisualStyleBackColor = true;
+            // 
+            // toolStrip1
+            // 
+            this.toolStrip1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.toolStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.PathLabel});
+            this.toolStrip1.Location = new System.Drawing.Point(142, 336);
+            this.toolStrip1.Name = "toolStrip1";
+            this.toolStrip1.Size = new System.Drawing.Size(535, 25);
+            this.toolStrip1.TabIndex = 1;
+            this.toolStrip1.Text = "toolStrip1";
+            // 
+            // CanvasBox
+            // 
+            this.CanvasBox.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("CanvasBox.BackgroundImage")));
+            this.CanvasBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.CanvasBox.Location = new System.Drawing.Point(155, 10);
+            this.CanvasBox.Name = "CanvasBox";
+            this.CanvasBox.Size = new System.Drawing.Size(510, 317);
+            this.CanvasBox.TabIndex = 2;
+            this.CanvasBox.TabStop = false;
+            // 
+            // PathLabel
+            // 
+            this.PathLabel.Name = "PathLabel";
+            this.PathLabel.Size = new System.Drawing.Size(22, 22);
+            this.PathLabel.Text = "C:/";
             // 
             // Source
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(584, 361);
+            this.ClientSize = new System.Drawing.Size(677, 361);
+            this.Controls.Add(this.CanvasBox);
+            this.Controls.Add(this.toolStrip1);
+            this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
             this.MaximizeBox = false;
             this.Name = "Source";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Map Extractor   |   Sakamochanq";
+            this.panel1.ResumeLayout(false);
+            this.toolStrip1.ResumeLayout(false);
+            this.toolStrip1.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.CanvasBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.ToolStrip toolStrip1;
+        private System.Windows.Forms.PictureBox CanvasBox;
+        private System.Windows.Forms.Button LoadShapeButton;
+        private System.Windows.Forms.ToolStripLabel PathLabel;
     }
 }
 
